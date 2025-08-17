8412d66a0752ed0897ed148c7253bac4446a15a0
ShreyasVij <shrvij28@gmail.com>
2025-08-17 16:34:34 +0530

    .

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ async function main() {
 const store = MongoStore.create({
     mongoUrl:dbUrl,
     crypto:{
-        secret:process.env.secret
+        secret:process.env.SECRET
     },
     touchAfter:24*3600,
 });
@@ -52,7 +52,7 @@ store.on("error",()=>{
 
 const sessionOption={
     store,
-    secret:process.env.secret,
+    secret:process.env.SECRET,
     resave:false,
     saveUninitialized: true,
     cookie:{
